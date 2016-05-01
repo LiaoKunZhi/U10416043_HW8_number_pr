1e73865c1fdef722f30c60673cd97537e3587fd1
LiaoKunZhi <a0956823675@gmail.com>
2016-05-01 21:13:17 +0800

    Create NumberPr.java-2
    Let the class NumberPr can calculate the two numbers, and return the result to the class TestNumberPr.

--- NumberPr.java
+++ NumberPr.java
@@ -3,7 +3,7 @@
 import java.math.*;
 
 //Class
-public class NumberPr {
+public class NumberPr{
 
 	//Data field
 	BigDecimal npr1;
@@ -13,5 +13,28 @@ public class NumberPr {
 	NumberPr (String newNpr1, String newNpr2) {
 		npr1 = new BigDecimal(newNpr1);
 		npr2 = new BigDecimal(newNpr2);
+	}
+	
+	//Method
+	String getCalculate (int want, int scale) {
+
+		String result = "";
+
+		if (want == 1) {
+			result = String.valueOf(npr1.add(npr2));
+		}			
+		else if (want == 2) {
+			result = String.valueOf(npr1.subtract(npr2));
+		}
+		else if (want == 3) {
+			result = String.valueOf(npr1.multiply(npr2));
+		}
+		else if (want == 4) {			
+			result = String.valueOf(npr1.divide(npr2,scale,BigDecimal.ROUND_HALF_UP));
+		}
+		else {
+			result = "ERROR";
+		}
+		return result;
 	}
 }
