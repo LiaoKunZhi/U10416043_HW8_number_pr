66a36c9b0c0ff3b1a3073bcfa676840981d3fc89
LiaoKunZhi <a0956823675@gmail.com>
2016-05-01 21:04:49 +0800

    Create TestNumberPr.java-3
    Let user can choose what he or she want to do to the two numbers he or she enter,and call the result from the class NumberPr.

--- TestNumberPr.java
+++ TestNumberPr.java
@@ -18,5 +18,17 @@ public class TestNumberPr{
 		
 		//Let the two number can be called from the class NumberPr
 		NumberPr n = new NumberPr(npr1,npr2);
-		}
+		
+		//Let user choose the method he or she want to do to the two numbers
+		System.out.println("Please enter what you want to do { (1)addition (2)subtract (3)multiply (4)divide }:");
+		int calculate = input.nextInt();
+		
+		//Let user to enter the scale he or she want
+		System.out.println("Please enter a scale ( >=0 ): ");
+		int scale = input.nextInt();
+		
+		//Show the result after calculate
+		System.out.println("Result: " + n.getCalculate(calculate,scale));
+		
+	}
 }
