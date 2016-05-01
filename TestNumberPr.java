b547cbd4b12c9f4609b3e0f01fdd41e7910e736e
LiaoKunZhi <a0956823675@gmail.com>
2016-05-01 21:02:27 +0800

    Create TestNumberPr.java-2
    Let user can enter two numbers to the class, and make them can be called from the class NumberPr.

--- TestNumberPr.java
+++ TestNumberPr.java
@@ -9,5 +9,14 @@ public class TestNumberPr{
 	public static void main(String[] args){
 
 		Scanner input = new Scanner(System.in);
+		
+		//Ask user to enter the two numbers he or she want
+		System.out.println("Please enter first number you want : ");
+		String npr1 = input.next();
+		System.out.println("Please enter second number you want : ");
+		String npr2 = input.next();
+		
+		//Let the two number can be called from the class NumberPr
+		NumberPr n = new NumberPr(npr1,npr2);
 		}
 }
