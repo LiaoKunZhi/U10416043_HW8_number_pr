58ec973c154892087a80edcf6cd20c415debac64
LiaoKunZhi <a0956823675@gmail.com>
2016-05-01 20:59:07 +0800

    Create TestNumberPr.java
    Make a new class to start the project, and put a new main method in it.

---
+++ TestNumberPr.java
@@ -0,0 +1,13 @@
+//U10416043   廖崑智
+
+import java.util.*;
+
+//Class
+public class TestNumberPr{
+
+	//Main method
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+		}
+}
