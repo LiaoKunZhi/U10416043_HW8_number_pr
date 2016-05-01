7741321f4153bb2cfaafe3de8f729d537e62d842
LiaoKunZhi <a0956823675@gmail.com>
2016-05-01 21:05:50 +0800

    Create NumberPr.java

---
+++ NumberPr.java
@@ -0,0 +1,17 @@
+//U10416043   廖崑智
+
+import java.math.*;
+
+//Class
+public class NumberPr{
+
+	//Data field
+	BigDecimal npr1;
+	BigDecimal npr2;
+	
+	//Constructor
+	NumberPr (String newNpr1, String newNpr2) {
+		npr1 = new BigDecimal(newNpr1);
+		npr2 = new BigDecimal(newNpr2);
+	}
+}
